2fe57a19525c4e6b391e8df57f16ae4af859b53e
Ken Wallace <rancoisse@gmail.com>
2025-11-09 11:09:51 +1100

    Lazy load routes

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <RouterLink to="/projects">Projects</RouterLink>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <div>
+        <h1>Projects Page</h1>
+        <RouterLink to="/">Home</RouterLink>
+    </div>
+</template>
+
+<style scoped></style>
