1e85b3fcf25243a2814d7bcb31ac6a3ca502b8cf
Ken Wallace <rancoisse@gmail.com>
2025-11-09 11:46:44 +1100

    Dynamic Routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const router = createRouter({
             name: 'projects',
             component: () => import('@/views/ProjectsView.vue'),
         },
+        {
+            path: '/projects/:id',
+            name: 'single-project',
+            component: () => import('@/views/SingleProjectView.vue'),
+        },
     ],
 })
 
