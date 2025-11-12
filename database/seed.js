a0d3ad5705f7b306b015e8252bdc257db885de1d
Ken Wallace <rancoisse@gmail.com>
2025-11-12 18:39:06 +1100

    Query supabase

--- database/seed.js
+++ database/seed.js
@@ -1,25 +1,49 @@
 /* eslint-env node */
 
-import { fakerEN_AU as faker } from '@faker-js/faker'
+import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-const seedProjects = async (recordCount) => {
+const logErrorAndExit = (tableName, error) => {
+    console.error(
+        `An error occurred in table '${tableName}' with code ${error.code}: ${error.message}`,
+    )
+    process.exit(1)
+}
+
+const logStep = (stepMessage) => {
+    console.log(stepMessage)
+}
+
+const seedProjects = async (numEntries) => {
+    logStep('Seeding projects...')
     const projects = []
 
-    for (let i = 0; i < recordCount; i++) {
+    for (let i = 0; i < numEntries; i++) {
         const name = faker.lorem.words(3)
 
         projects.push({
             name: name,
-            slug: faker.helpers.slugify(name),
+            slug: name.toLocaleLowerCase().replace(/ /g, '-'),
             status: faker.helpers.arrayElement(['in-progress', 'completed']),
-            collaborators: faker.helpers.arrayElements([1, 2, 3, 4, 5]),
+            collaborators: faker.helpers.arrayElements([1, 2, 3]),
         })
     }
 
-    await supabase.from('projects').insert(projects)
+    const { data, error } = await supabase.from('projects').insert(projects).select('id')
+
+    if (error) return logErrorAndExit('Projects', error)
+
+    logStep('Projects seeded successfully.')
+
+    return data
 }
 
-await seedProjects(12)
+const seedDatabase = async (numEntriesPerTable) => {
+    await seedProjects(numEntriesPerTable)
+}
+
+const numEntriesPerTable = 10
+
+seedDatabase(numEntriesPerTable)
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { supabase } from '@/lib/supabaseClient'
+
+const getProjects = async () => {
+    const { data, error } = await supabase.from('projects').select()
+
+    if (error) console.log(error)
+
+    console.log('Projects: ', data)
+}
 </script>
 
 <template>
