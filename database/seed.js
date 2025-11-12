a0d971476ae70f5eb75573da9bc48e4beced970d
Ken Wallace <rancoisse@gmail.com>
2025-11-12 18:14:36 +1100

    Bulk seed projects

--- database/seed.js
+++ database/seed.js
@@ -5,15 +5,21 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-const seedProjects = async () => {
-    const name = faker.lorem.words(3)
+const seedProjects = async (recordCount) => {
+    const projects = []
 
-    await supabase.from('projects').insert({
-        name: name,
-        slug: faker.helpers.slugify(name),
-        status: faker.helpers.arrayElement(['in-progress', 'completed']),
-        collaborators: faker.helpers.arrayElements([1, 2, 3, 4, 5]),
-    })
+    for (let i = 0; i < recordCount; i++) {
+        const name = faker.lorem.words(3)
+
+        projects.push({
+            name: name,
+            slug: faker.helpers.slugify(name),
+            status: faker.helpers.arrayElement(['in-progress', 'completed']),
+            collaborators: faker.helpers.arrayElements([1, 2, 3, 4, 5]),
+        })
+    }
+
+    await supabase.from('projects').insert(projects)
 }
 
-await seedProjects()
+await seedProjects(12)
