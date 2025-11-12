02f3256b01040ab394610d839770b0dc2b5d8bde
Ken Wallace <rancoisse@gmail.com>
2025-11-12 16:57:33 +1100

    Supabase javascript client

---
+++ database/seed.js
@@ -0,0 +1,8 @@
+/* eslint-env node */
+
+import { fakerEN_AU as faker } from '@faker-js/faker'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+
+console.log(supabase)
