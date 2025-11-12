545c1ed793779bd5538e3407fb42332a26bb12bf
Ken Wallace <rancoisse@gmail.com>
2025-11-12 17:41:02 +1100

    Faker seed

--- database/seed.js
+++ database/seed.js
@@ -5,4 +5,15 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-console.log(supabase)
+const seedProjects = async () => {
+    const name = faker.lorem.words(3)
+
+    await supabase.from('projects').insert({
+        name: name,
+        slug: faker.helpers.slugify(name),
+        status: faker.helpers.arrayElement(['in-progress', 'completed']),
+        collaborators: faker.helpers.arrayElements([1, 2, 3, 4, 5]),
+    })
+}
+
+await seedProjects()
