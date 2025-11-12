3a07bbe061af30f44026dbf120eb3ed63db5f4ff
Ken Wallace <rancoisse@gmail.com>
2025-11-12 19:14:05 +1100

    Reactivity

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
-import { supabase } from '@/lib/supabaseClient'
-;(async () => {
-    const { data, error } = await supabase.from('projects').select()
-
-    if (error) console.log(error)
-
-    console.log('Projects: ', data)
-})()
 </script>
 
 <template>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+
+const projects = ref()
+;(async () => {
+    const { data, error } = await supabase.from('projects').select()
+
+    if (error) console.log(error)
+
+    projects.value = data
+
+    console.log('Projects: ', projects.value)
+})()
 </script>
 
 <template>
     <div>
         <h1>Projects Page</h1>
         <RouterLink to="/">Home</RouterLink>
+        {{ projects }}
     </div>
 </template>
 
