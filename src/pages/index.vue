f6d165ace2d1df382b7332c875703f9363481839
Ken Wallace <rancoisse@gmail.com>
2025-11-14 17:10:53 +1100

    Install shadcn

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
+    <div>
+        <Button>Click me</Button>
+    </div>
     <div>
         <h1>Home Page</h1>
         <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Project 1</RouterLink>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,7 +9,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [VueRouter(), vue(), vueDevTools()],
+    plugins: [VueRouter(), vue(), vueDevTools(), tailwindcss()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
