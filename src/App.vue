5c2b79a7496a92ac75973523d2fd82254e123261
Ken Wallace <rancoisse@gmail.com>
2025-11-19 18:24:47 +1100

    Lucide icons and iconify

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,11 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 <template>
     <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
-        <form class="w-full max-w-96">
+        <form class="relative w-full h-fit max-w-96">
+            <iconify-icon
+                class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground"
+                icon="lucide:search"
+            ></iconify-icon>
             <Input class="w-full pl-8 bg-background" type="text" placeholder="Search..." />
         </form>
         <DropdownMenu>
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [VueRouter(), vue(), vueDevTools(), tailwindcss()],
+    plugins: [
+        VueRouter(),
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (element) => element.startsWith('iconify-icon'),
+                },
+            },
+        }),
+        vueDevTools(),
+        tailwindcss(),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
