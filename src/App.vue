4a985aa0a94f355c920199612e5be934472a218f
Ken Wallace <rancoisse@gmail.com>
2025-11-29 17:06:40 +1100

    Enhance suspense component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
-<script setup lang="ts">
-const MockComponent = defineComponent(async () => {
-    const greeting = ref('Hello Sweetie')
-
-    await new Promise((resolve) => {
-        greeting.value = "It's the database here"
-
-        setTimeout(() => {
-            resolve(true)
-        }, 1000)
-    })
-
-    return () => h('p', greeting.value)
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <AuthLayout>
-        <Suspense>
-            <MockComponent />
-            <template #fallback>
-                <span>Loading ...</span>
-            </template>
-        </Suspense>
-        <!-- <RouterView /> -->
+        <RouterView v-slot="{ Component, route }">
+            <Suspense v-if="Component" :timeout="0">
+                <Component :is="Component" :key="route.name"></Component>
+
+                <template #fallback>
+                    <span>Loading ...</span>
+                </template>
+            </Suspense>
+        </RouterView>
     </AuthLayout>
 </template>
--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -6,13 +6,15 @@ import type { ColumnDef } from '@tanstack/vue-table'
 import DataTable from '@/components/ui/data-table/DataTable.vue'
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
-;(async () => {
+const getTasks = async () => {
     const { data, error } = await supabase.from('tasks').select()
 
     if (error) console.log(error)
 
     tasks.value = data
-})()
+}
+
+await getTasks()
 
 const columns: ColumnDef<Tables<'tasks'>>[] = [
     {
