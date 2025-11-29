8de28620228e5db9e9c681cb6642435b483051c0
Ken Wallace <rancoisse@gmail.com>
2025-11-29 16:36:41 +1100

    Suspense component async dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const MockComponent = defineComponent(async () => {
+    const greeting = ref('Hello Sweetie')
+
+    await new Promise((resolve) => {
+        greeting.value = "It's the database here"
+
+        setTimeout(() => {
+            resolve(true)
+        }, 1000)
+    })
+
+    return () => h('p', greeting.value)
+})
+</script>
 
 <template>
     <AuthLayout>
-        <RouterView />
+        <Suspense>
+            <MockComponent />
+            <template #fallback>
+                <span>Loading ...</span>
+            </template>
+        </Suspense>
+        <!-- <RouterView /> -->
     </AuthLayout>
 </template>
