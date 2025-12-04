0f869abfc79738064a589644f393551266aa351f
Ken Wallace <rancoisse@gmail.com>
2025-12-04 17:37:18 +1100

    Individual project page

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -1,11 +1,115 @@
 <script setup lang="ts">
+import { projectQuery } from '@/utilities/supaQueries'
+import type { Project } from '@/utilities/supaQueries'
+
 const route = useRoute('/projects/[slug]')
+
+const project = ref(<Project | null>null)
+
+const getProjects = async () => {
+    const { data, error } = await projectQuery(route.params.slug)
+
+    if (error) console.log(error)
+
+    project.value = data
+}
+
+await getProjects()
 </script>
 
 <template>
-    <div>
-        <h1>Project {{ route.params?.slug }}</h1>
-    </div>
+    <Table>
+        <TableRow>
+            <TableHead> Name </TableHead>
+            <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+        </TableRow>
+        <TableRow>
+            <TableHead> Description </TableHead>
+            <TableCell> Lorem ipsum dolor sit amet, consectetur adipisicing elit. </TableCell>
+        </TableRow>
+        <TableRow>
+            <TableHead> Status </TableHead>
+            <TableCell>In progress</TableCell>
+        </TableRow>
+        <TableRow>
+            <TableHead> Collaborators </TableHead>
+            <TableCell>
+                <div class="flex">
+                    <Avatar
+                        class="-mr-4 border border-primary hover:scale-110 transition-transform"
+                        v-for="n in 5"
+                        :key="n"
+                    >
+                        <RouterLink class="w-full h-full flex items-center justify-center" to="">
+                            <AvatarImage src="" alt="" />
+                            <AvatarFallback> </AvatarFallback>
+                        </RouterLink>
+                    </Avatar>
+                </div>
+            </TableCell>
+        </TableRow>
+    </Table>
+
+    <section class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
+        <div class="flex-1">
+            <h2>Tasks</h2>
+            <div class="table-container">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead> Name </TableHead>
+                            <TableHead> Status </TableHead>
+                            <TableHead> Due Date </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow v-for="n in 5" :key="n">
+                            <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+                            <TableCell> In progress </TableCell>
+                            <TableCell> 22/08/2024 </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
+        <div class="flex-1">
+            <h2>Documents</h2>
+            <div class="table-container">
+                <p class="text-muted-foreground text-sm font-semibold px-4 py-3">
+                    This project doesn't have documents yet...
+                </p>
+                <!-- <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead> Name </TableHead>
+              <TableHead> Visibility </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+              <TableCell> Private </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table> -->
+            </div>
+        </div>
+    </section>
 </template>
 
+<style>
+@reference "../../assets/index.css";
+
+th {
+    @apply w-[100px];
+}
+
+h2 {
+    @apply mb-4 text-lg font-semibold w-fit;
+}
+
+.table-container {
+    @apply overflow-hidden overflow-y-auto rounded-md bg-white h-80;
+}
+</style>
 <style scoped></style>
